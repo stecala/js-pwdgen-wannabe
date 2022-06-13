d6e5f54dba8bbe136b98b20a1d26c7cf30636b3f
stecala <ste.cala123@gmail.com>
2022-06-13 13:44:09 +0200

    bonus control

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 const userName = prompt('Inserisci il tuo nome');
 const userSurname = prompt('Inserisci il tuo cognome');
 const userColor = prompt('Inserisci il tuo colore preferito');
-
-document.getElementById('password').innerHTML = userName + userSurname + userColor + 22;
+if( userName.substring.length <= 3 || userSurname <= 2 )
+{
+    alert('nome o cognome troppo corto')
+}
+else
+{
+    document.getElementById('password').innerHTML = userName + userSurname + userColor + 22;
+}
