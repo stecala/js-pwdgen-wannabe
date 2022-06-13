f7cbf200233eca7a53bbf0792684402d7e6665c0
stecala <ste.cala123@gmail.com>
2022-06-13 13:43:45 +0200

    end exercise

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const userName = prompt('Inserisci il tuo nome');
+const userSurname = prompt('Inserisci il tuo cognome');
+const userColor = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('password').innerHTML = userName + userSurname + userColor + 22;
